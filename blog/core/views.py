643094f5f85cb2fc20ef3b0ba9916b0bea2db13a
Ruppy <ruppysuppy@gmail.com>
2020-01-18 21:07:44 +0530

    Added ordering (filled with bugs till now)

--- blog/core/views.py
+++ blog/core/views.py
@@ -9,6 +9,7 @@ from flask import render_template, request, Blueprint
 ####################################################
 
 from blog.models import BlogPost
+from blog import db
 
 ####################################################
 # BLUEPRINT SETUP ##################################
@@ -23,7 +24,19 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+
+    try:
+        query1 = db.engine.execute(f"select * \
+                                        from BlogPost \
+                                        where category='{current_user.last_viewed_catagory}'")
+        query2 = db.engine.execute(f"select * \
+                                        from BlogPost \
+                                        where category!='{current_user.last_viewed_catagory}'")
+        blog_posts = query1.union(query2)
+    except:
+        blog_posts = BlogPost.query.order_by(BlogPost.views.desc())
+    
+    blog_posts = blog_posts.paginate(page=page, per_page=5)
     return render_template('index.html', page_name="Home", blog_posts=blog_posts)
 
 ####################################################
--- blog/users/views.py
+++ blog/users/views.py
@@ -115,6 +115,6 @@ def account():
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.views.desc()).paginate(page=page, per_page=5)
 
     return render_template('user_blog_posts.html', user=user, blog_posts=blog_posts)
